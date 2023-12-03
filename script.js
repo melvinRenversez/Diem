e6d75eebaecb7ccd76eacb342a912a8f5ebebb0c
melvinRenversez <120313822+melvinRenversez@users.noreply.github.com>
2023-12-04 00:19:50 +0100

    a

---
+++ script.js
@@ -0,0 +1,174 @@
+canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
+
+const content = document.getElementById("content")
+const info = document.getElementById("OkValue")
+
+const nomberStart = 30;
+const nomberClone = 50;
+const maxSize = 30;
+
+var OK = true;
+var view = false
+var shape = "cube"
+
+function Particle(x, y, speedX, speedY, size, color, type, intervalReproducrion, lifeTime) {
+
+    this.x = x
+    this.y = y
+    this.speedX = speedX
+    this.speedY = speedY
+    this.size = size
+    this.color = color
+    this.lifeTime = lifeTime
+    this.type = type
+    this.intervalReproducrion = intervalReproducrion
+    this.lastReproduction = new Date().getTime()
+
+    this.draw = function(){
+        if (shape == "cube"){
+            ctx.fillStyle = this.color;
+            ctx.fillRect(this.x, this.y, this.size, this.size)
+        }else{
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x + this.size / 2, this.y + this.size / 2, this.size / 2, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
+    this.move = function(){
+        this.x += this.speedX
+        this.y += this.speedY
+
+
+        if (this.x < 0 || this.x + this.size > canvas.width){
+            this.speedX *= -1
+        }
+        if (this.y < 0 || this.y + this.size > canvas.height) {
+            this.speedY *= -1
+        }
+
+
+    }
+
+    this.life = function(){
+        if (this.type == "clone"){
+            this.lifeTime -= 1
+            if (this.lifeTime <= 0){
+                const index = Particles.indexOf(this)
+                if (index !== -1) {
+                    Particles.splice(index, 1)
+                    ctx.clearRect(this.x, this.y, this.size, this.size)
+                }
+            }
+        }
+    }
+
+    this.reproduction = function(){
+        if (OK == true){
+            if ( this.type === "parent"){
+                date = new Date().getTime()
+                if (date - this.lastReproduction > this.intervalReproducrion){
+                    this.lastReproduction = date
+                    for (var i = 0; i < nomberClone; i++){
+                        newParticle(this.x, this.y)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+function newParticle(x, y){
+
+    const speedX = (Math.random() - 0.5) * 20
+    const speedY = (Math.random() - 0.5) * 20
+
+    const lifetime = Math.random() * 100
+
+    color = Math.random() < 0.5 ? "red" : "white"
+
+    const size = Math.random() * maxSize
+
+    Particles.push(new Particle(x, y, speedX, speedY, size, color, "clone", NaN, lifetime))
+
+    
+}
+
+
+const Particles = []
+
+for (var i = 0; i < nomberStart; i++){
+
+    const x = Math.random() * canvas.width
+    const y = Math.random() * canvas.height
+
+    const speedX = (Math.random() - 0.5) * 10
+    const speedY = (Math.random() - 0.5) * 10
+
+    const intervalReproduction = 5000 * (Math.random() + 0.5)
+
+    Particles.push(new Particle(x, y, speedX, speedY, 10, "blue", "parent", intervalReproduction))
+
+}
+
+function loop(){
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    for (var i = 0; i < Particles.length; i++){
+        Particles[i].move()
+        Particles[i].draw()
+        Particles[i].reproduction()
+        Particles[i].life()
+    }
+
+    if (view == true){
+        content.style.visibility = "visible"
+        t = new Date().getTime()
+        if (t - affichageTime > 1000){
+            view = false
+            content.style.visibility = "hidden"
+        }
+    }
+
+    requestAnimationFrame(loop);
+
+}
+
+var affichageTime;
+
+document.addEventListener("keydown", function(e){
+
+    console.log(e.keyCode)
+    if (e.keyCode === 68){
+        if (OK == true){
+            OK = false
+            info.innerHTML = " False"
+            view = true
+            affichageTime = new Date().getTime()
+        }else{
+            OK = true
+            info.innerHTML = " True"
+            view = true
+            affichageTime = new Date().getTime()
+        }
+    }
+
+    if (e.keyCode == 32){
+        for (i = 0; i < Particles.length; i++) {
+            if (Particles[i].type == "parent"){
+                if (Particles[i].color == "blue"){
+                    Particles[i].color = "black"
+                }else{
+                    Particles[i].color = "blue"
+                }
+            }
+        }
+    }
+})
+
+
+loop()
